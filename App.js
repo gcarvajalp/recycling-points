55ad349df3903ef8c224416eb295b504360012bf
gabriel carvajal pizarro <g.carvajal.pizarro@gmail.com>
2020-02-18 22:05:00 -0300

    test flex dimensions

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
 
-const styles = StyleSheet.create({
-	bigBlue:{
-		color: 'blue',
-		fontWeight: 'bold',
-		fontSize:30,
-	},
-	red:{
-		color:'red',
-	}
-});
-
-export default class LotsOfStyles extends Component{
+export default class FixedDimensionesBasics extends Component{
 	render(){
 		return(
 			<View>
-				<Text style={styles.red}>just Red</Text>
-				<Text style={styles.bigBlue}>just bigBlue</Text>
-				<Text style={[styles.bigBlue, styles.red]}>big blue, then red</Text>
-				<Text style={[styles.red, styles.bigBlue]}>red, then big blue</Text>
+				<View style={{width:50, height:50, backgroundColor:'powderblue'}}/>
+				<View style={{width: 100, height:100, backgroundColor:'skyblue'}}/>
+				<View style={{width: 150, height:150, backgroundColor:'steelblue'}}/>
 			</View>
 		);
 	}
