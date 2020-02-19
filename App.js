928d17f9f74c5bf6518184f66cb65ce6d905af59
gabriel carvajal pizarro <g.carvajal.pizarro@gmail.com>
2020-02-18 23:05:47 -0300

    example about flexDirection

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 
-export default class FixedDimensionesBasics extends Component{
+export default class FlexDirectionsBasics extends Component{
 	render(){
 		return(
-			<View>
+			<View style={{flex:1, flexDirection:'column-reverse'}}>
 				<View style={{width:50, height:50, backgroundColor:'powderblue'}}/>
-				<View style={{width: 100, height:100, backgroundColor:'skyblue'}}/>
-				<View style={{width: 150, height:150, backgroundColor:'steelblue'}}/>
+				<View style={{width:50, height:50, backgroundColor:'skyblue'}}/>
+				<View style={{width:50, height:50, backgroundColor:'steelblue'}}/>
 			</View>
 		);
 	}
-}
+};
