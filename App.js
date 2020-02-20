1b7502fd0a2dac8b871a191fc386d8dc422785c3
gabriel carvajal pizarro <g.carvajal.pizarro@gmail.com>
2020-02-19 21:51:39 -0300

    add button basics examples

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
 
-export default class FlexDirectionsBasics extends Component{
+export default class buttonBasics extends Component {
+	_onPressButton(){
+		alert('You tapped the button!');
+	}
+
 	render(){
 		return(
-			<View style={{flex:1, flexDirection:'column-reverse'}}>
-				<View style={{width:50, height:50, backgroundColor:'powderblue'}}/>
-				<View style={{width:50, height:50, backgroundColor:'skyblue'}}/>
-				<View style={{width:50, height:50, backgroundColor:'steelblue'}}/>
+			<View style={styles.container}>
+				<View style={styles.buttonContainer}>
+				<Button onPress={this._onPressButton} title="Press Me"/>
+				</View>
+				<View style={styles.buttonContainer}>
+					<Button onPress={this._onPressButton} title="Press Me" color="#841584"/>
+				</View>
+				<View style={styles.alternativeLayoutButtonContainer}>
+				<Button onPress={this._onPressButton} title="This looks great!" />
+				<Button onPress={this._onPressButton} title="OK!" color="#841584" />
+				</View>
 			</View>
 		);
 	}
-};
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex :1 ,
+		justifyContent: 'center'
+	},
+	buttonContainer:{
+		margin:20
+	},
+	alternativeLayoutButtonContainer:{
+		margin:20,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	}
+});
